00c715161823bd1f8324a633d7921f3748defb10
hfilipe- <hfilipe-@student.42porto.com>
2025-05-26 19:22:54 +0100

    imrpove test file

--- src/test_map.c
+++ src/test_map.c
@@ -16,14 +16,13 @@ char    **test_Array(t_cub *cub)
 
 	cub->textures = malloc(sizeof(char *) * 5);
 	for (size_t i = 0; i < 4; i++)
-		cub->textures[i] = malloc(sizeof(char) * 2);
-	cub->textures[0][0] = 'N';
-	cub->textures[1][0] = 'S';
-	cub->textures[2][0] = 'E';
-	cub->textures[3][0] = 'W';
-	for (size_t i = 0; i < 4; i++)
-		cub->textures[i][1] = '\0';
-	cub->textures[5] = 0;
+		cub->textures[i] = malloc(sizeof(char) * 50);
+
+	cub->textures[0] = ft_strdup("textures/N");
+	cub->textures[1] = ft_strdup("textures/S");
+	cub->textures[2] = ft_strdup("textures/E");
+	cub->textures[3] = ft_strdup("textures/W");
+	cub->textures[4] = 0;
 
 	cub->ceiling.r = 20;
 	cub->ceiling.g = 2;
