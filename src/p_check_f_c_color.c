4ae9934709c150cc45271418406c7d6f6058f8f5
Hugo Lopes <hugofslopes@live.com.pt>
2025-06-13 11:17:04 +0100

    Creat atoi cube

--- src/p_check_f_c_color.c
+++ src/p_check_f_c_color.c
@@ -3,14 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   p_check_f_c_color.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pauldos- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hfilipe- <hfilipe-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 09:41:04 by pauldos-          #+#    #+#             */
-/*   Updated: 2025/06/11 09:41:06 by pauldos-         ###   ########.fr       */
+/*   Updated: 2025/06/13 11:15:36 by hfilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+int	cub_atoi(const char *str)
+{
+	int		sign;
+	int		result;
+	char	**nstr;
+	char	*n_str;
+	char	*n_str2;
+	
+	nstr = ft_split(str, ',');
+	n_str2 = ft_strtrim(*nstr, " ");
+	n_str = ft_strtrim(n_str2, "\t");
+	if (ft_strlen(n_str) > 4)
+		return (free(n_str), free(n_str2), free_array(nstr), -1);
+	sign = 1;
+	result = 0;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+		if (*str++ == '-')
+			sign *= -1;
+	while (*str && *str >= '0' && *str <= '9')
+		result = (result * 10) + (*str++ - '0');
+	return (free(n_str), free(n_str2), free_array(nstr),result * sign);
+}
+
 
 int	handle_floor(t_config *cfg, t_config_flags *flags, char *value)
 {
@@ -40,7 +66,7 @@ int	parse_single_rgb(char **ptr, int *val)
 		(*ptr)++;
 	if (!ft_isdigit(**ptr))
 		return (0);
-	*val = ft_atoi(*ptr);
+	*val = cub_atoi(*ptr);
 	if (*val < 0 || *val > 255)
 		return (0);
 	while (ft_isdigit(**ptr))
